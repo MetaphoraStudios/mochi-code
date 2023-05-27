4f0ca6cca1791bd0fdf5d7163dcef952329a1d91
Diogo Neves <diogo.neves@gmail.com>
2023-05-28 00:39:43 +0100

    Sets ask command with tests

--- mochi_code/ask.py
+++ mochi_code/ask.py
@@ -1,3 +1,4 @@
+from typing import Optional, Sequence
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
@@ -10,15 +11,32 @@ import argparse
 keys = dotenv_values(".keys")
 
 
-def main(user_query: str):
-    chain = create_chain()
-    print(chain.run(user_query))
+class EmptyPromptError(Exception):
+    pass
 
 
-def create_chain() -> LLMChain:
+def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", type=str, help="Your non-empty prompt to run.")
+    args = parser.parse_args()
+
+    prompt = args.prompt
+    if not prompt.strip():
+        parser.print_help()
+        raise EmptyPromptError("Prompt cannot be empty.")
+
+    ask(prompt)
+
+
+def ask(prompt: str):
+    chain = _create_chain()
+    print(chain.run(prompt))
+
+
+def _create_chain() -> LLMChain:
     llm = OpenAI(temperature=0.9, openai_api_key=keys["OPENAI_API_KEY"])  # type: ignore
     prompt = PromptTemplate(
-        input_variables=["query"],
+        input_variables=["user_prompt"],
         template="""You are an great software engineer helping other engineers.
         Whenever possible provide code examples, prioritise copying code from 
         the following prompt (if available). If you're creating a function or 
@@ -26,20 +44,6 @@ def create_chain() -> LLMChain:
         Keep answers related to code, if you think the query is not related to 
         code, please ask to clarify, provide more context or rephrase the query,
         but keep it very polite and friendly, or create a pun with it.
-        Please answer this query: '{query}'""",
+        Please answer this query: '{user_prompt}'""",
     )
     return LLMChain(llm=llm, prompt=prompt)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("prompt", type=str, help="Your non-empty prompt to run.")
-    args = parser.parse_args()
-
-    prompt = args.prompt
-    if not prompt.strip():
-        print("Prompt cannot be empty.")
-        parser.print_help()
-        exit(1)
-
-    main(prompt)
---
+++ tests/test_ask.py
@@ -0,0 +1,32 @@
+from unittest import TestCase
+from unittest.mock import patch
+from mochi_code.ask import cli, EmptyPromptError
+from pytest import raises, MonkeyPatch
+
+
+class TestAskCli(TestCase):
+    """Test the cli function in ask.py"""
+
+    @patch("mochi_code.ask.ask")
+    def test_empty_prompt_fails(self, mock_ask):
+        """Test that an empty prompt fails."""
+        mock_ask.return_value = None
+
+        MonkeyPatch().setattr("sys.argv", ["mochi", ""])
+        with raises(EmptyPromptError):
+            cli()
+
+        MonkeyPatch().setattr("sys.argv", ["mochi", "         "])
+        with raises(EmptyPromptError):
+            cli()
+
+    @patch("mochi_code.ask.ask")
+    def test_non_empty_prompt_succeeds(self, mock_ask):
+        """Test that a non-empty prompt succeeds."""
+        mock_ask.return_value = None
+
+        prompt = "test"
+        MonkeyPatch().setattr("sys.argv", ["mochi", prompt])
+        cli()
+
+        mock_ask.assert_called_once_with(prompt)
