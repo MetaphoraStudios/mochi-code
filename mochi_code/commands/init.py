833e088df5c86e247b7844e01a7f528a30aa0859
Diogo Neves <diogo.neves@gmail.com>
2023-06-06 00:35:51 +0100

    Sketching traversal

--- mochi_code/commands/init.py
+++ mochi_code/commands/init.py
@@ -2,6 +2,8 @@
 project."""
 
 import argparse
+import pathlib
+from typing import Optional
 
 
 def setup_init_arguments(parser: argparse.ArgumentParser) -> None:
@@ -22,3 +24,24 @@ def run_init_command(args: argparse.Namespace) -> None:
 def init() -> None:
     """Run the init command."""
     raise NotImplementedError()
+
+
+def _get_existing_mochi_config(
+        path: pathlib.Path) -> Optional[pathlib.PurePath]:
+    """Get the existing mochi config file if it exists.
+    This will search up the directory tree from the provided path until it finds
+    a mochi config file or reaches the root directory.
+
+    Args:
+        path (pathlib.Path): The path to start searching from.
+
+    Returns:
+        Optional[pathlib.PurePath]: The path to the mochi config file or None if
+            it does not exist.
+    """
+    current_path = path
+    while current_path != current_path.root:
+        mochi_config_path = current_path / ".mochi"
+        if mochi_config_path.exists():
+            return mochi_config_path
+        current_path = current_path.parent
--- tests/commands/test_init.py
+++ tests/commands/test_init.py
@@ -9,3 +9,28 @@ class TestSetupInitArguments(TestCase):
 
 class TestRunInitCommand(TestCase):
     """Test the run_init_command function."""
+
+
+class TestGetExistingMochiConfig(TestCase):
+    """Test the _get_existing_mochi_config function."""
+
+    # TODO: Mock the path.root value
+    def test_starting_at_root_not_found(self):
+        """Test that the function returns None when starting at the root and
+        there isn't a mochi config."""
+
+    def test_starting_at_root_found(self):
+        """Test that the function returns root path when starting at the root
+        and there is a mochi config."""
+
+    def test_finds_in_parent(self):
+        """Test that the function returns the parent path when there is a mochi
+        config in the parent."""
+
+    def test_finds_in_self(self):
+        """Test that the function returns the current path when there is a mochi
+        config in the current path."""
+
+    def test_not_found_across_dir_tree(self):
+        """Test that the function returns None when there is no mochi config
+        across the directory tree."""
