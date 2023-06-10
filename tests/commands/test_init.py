6c23fd5238c7eb90d532e694890f826c1cb7d0b8
Diogo Neves <diogo.neves@gmail.com>
2023-06-10 10:52:26 +0100

    Implements tests

--- tests/commands/test_init.py
+++ tests/commands/test_init.py
@@ -2,6 +2,7 @@
 
 import argparse
 import pathlib
+import tempfile
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
@@ -16,32 +17,37 @@ class TestSetupInitArguments(TestCase):
 class TestRunInitCommand(TestCase):
     """Test the run_init_command function."""
 
-    @patch("mochi_code.code.mochi_config.search_mochi_config")
+    @patch("mochi_code.commands.init.search_mochi_config")
     @patch("mochi_code.commands.init.init")
-    def test_it_raises_if_already_initialized(self, search_mock: MagicMock,
-                                              mock_init: MagicMock) -> None:
+    @patch("mochi_code.commands.init.pathlib.Path.cwd")
+    def test_it_raises_if_already_initialized(self, mock_cwd: MagicMock, mock_init: MagicMock,
+                                              mock_search: MagicMock, ) -> None:
         """Test that the function raises if the project is already initialized.
         """
-        test_path = pathlib.Path("/some/path")
-        search_mock.return_value = test_path / ".mochi"
+        start_path = pathlib.Path("/some/path")
+
+        mock_cwd.return_value = start_path
         mock_init.return_value = None
+        mock_search.return_value = start_path / ".mochi"
 
-        with patch.object(pathlib.Path, "cwd",
-                          lambda: pathlib.Path("/some/path")):
-            error_pattern = rf".*'{test_path}'."
-            with self.assertRaisesRegex(MochiCannotContinue, error_pattern):
-                run_init_command(argparse.Namespace())
+        error_pattern = rf".*'{start_path}'."
+        with self.assertRaisesRegex(MochiCannotContinue, error_pattern):
+            run_init_command(argparse.Namespace())
+        mock_search.assert_called_once()
 
-    @patch("mochi_code.code.mochi_config.search_mochi_config")
+    @patch("mochi_code.commands.init.search_mochi_config")
     @patch("mochi_code.commands.init.init")
-    def test_it_calls_init(self, search_mock: MagicMock,
-                           mock_init: MagicMock) -> None:
+    @patch("mochi_code.commands.init.pathlib.Path.cwd")
+    def test_it_calls_init(self, mock_cwd: MagicMock, mock_init: MagicMock,
+                           search_mock: MagicMock) -> None:
         """Test that the function calls init with the project path."""
+        start_path = pathlib.Path(tempfile.TemporaryDirectory().name)
+
         search_mock.return_value = None
         mock_init.return_value = None
+        mock_cwd.return_value = start_path
 
-        start_path = pathlib.Path("/some/path")
-        with patch.object(pathlib.Path, "cwd", lambda: start_path):
-            run_init_command(argparse.Namespace())
-            assert mock_init.call_count == 1
-            mock_init.assert_called_once_with(start_path)
+        run_init_command(argparse.Namespace())
+
+        assert mock_init.call_count == 1
+        mock_init.assert_called_once_with(start_path)
